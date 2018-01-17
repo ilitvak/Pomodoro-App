7c8f064713f45f3fd84fcd8bf3ed48dc9e07a3d1
Irwin <irwin.litvak@gmail.com>
2018-01-17 00:51:33 -0500

    restructured files and folder structure for github pages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,21 +14,21 @@ TimerApp.config(['$stateProvider', '$locationProvider', function($stateProvider,
   $stateProvider.state('longbreak', {
     url: '/longbreak',
     controller: 'ChillCtrl',
-    templateUrl: '/templates/longbreak.html'
+    templateUrl: '/app/templates/longbreak.html'
     });
 
 
   $stateProvider.state('work', {
     url: '/work',
     controller: 'WorkCtrl',
-    templateUrl: '/templates/work.html'
+    templateUrl: '/app/templates/work.html '
     });
 
 
   $stateProvider.state('break', {
     url: '/break',
     controller: 'BreakCtrl',
-    templateUrl: '/templates/break.html'
+    templateUrl: '/app/templates/break.html '
     }); 
   
 
